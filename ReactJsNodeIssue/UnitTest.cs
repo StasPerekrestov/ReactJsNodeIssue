ec32439129ab1360b429868100230311164a2f1b
Stanislav Perekrestov <stanislav.perekrestov@muranosoft.com>
2016-03-11 12:44:16 +0200

    simplify the test case

--- ReactJsNodeIssue/UnitTest.cs
+++ ReactJsNodeIssue/UnitTest.cs
@@ -4,15 +4,16 @@ using React;
 
 namespace ReactJsNodeIssue {
 	[TestClass]
-	public class UnitTest {
-		private const string TestJsxCode = 
-			"for (let i = 0; i < items['hazards'].length(); i++) {"+
-					"fq.then(function(cb) { "+
-						"fillHazard(items['hazards'].items(i), cb);"+
-					"}).then(fillAvailableClasses);"+
-			"}";
+	public class UnitTest
+	{
+		private const string TestJsxCode =
+		@"for(var i =0; i<3; ++i) {
+			let a = i;
+			$('#login-id').click(e => a = a + 1);
+		}";
 
-		private static void Initialize() {
+		private static void Initialize()
+		{
 			Initializer.Initialize(registration => registration.AsSingleton());
 			var container = React.AssemblyRegistration.Container;
 
@@ -21,12 +22,14 @@ namespace ReactJsNodeIssue {
 		}
 
 		[TestInitialize]
-		public void SetUp() {
+		public void SetUp()
+		{
 			Initialize();
 		}
 
 		[TestMethod]
-		public void Test() {
+		public void Test()
+		{
 			IBabel babel = ReactEnvironment.Current.Babel;
 			string transformedJs = babel.Transform(TestJsxCode);
 			Assert.IsNotNull(transformedJs);
